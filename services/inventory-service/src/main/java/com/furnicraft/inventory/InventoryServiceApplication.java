8cc6f29cc19c6e589dfe4a8886996fecfabc596d
Ali Hüseyn <ali.hsynov@gmail.com>
2026-01-18 19:22:02 +0400

    feat: initialize entry points for all microservices and infrastructure

---
+++ services/inventory-service/src/main/java/com/furnicraft/inventory/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.furnicraft.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+}
